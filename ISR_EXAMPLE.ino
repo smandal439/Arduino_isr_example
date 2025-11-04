ed06ff272c1c012e73af23502679ea54ba384bc2
Sushanta Mandal <154744591+smandal439@users.noreply.github.com>
2025-11-04 18:10:28 +0530

    Update ISR_EXAMPLE.ino

--- ISR_EXAMPLE.ino
+++ ISR_EXAMPLE.ino
@@ -1,1 +1,17 @@
 
+#define LED_PIN 13
+#define BUTTON_PIN 2
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), toggleLED, FALLING);
+}
+
+void loop() {
+  // Main loop keeps running
+}
+
+void toggleLED() {
+  digitalWrite(LED_PIN, !digitalRead(LED_PIN)); // Toggle LED state
+}
